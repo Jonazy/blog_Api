a54f185caee70b14a58ba26682a5cef740f173ba
John jonas <jonas.humenu@gmail.com>
2021-02-26 15:58:04 +0100

    update runtime

--- post/serializers.py
+++ post/serializers.py
@@ -3,24 +3,18 @@ from .models import Post, Comment, Like
 
 
 class PostSerializer(serializers.ModelSerializer):
-    # author = serializers.ReadOnlyField(source='author.first_name')
-    # comment_by = serializers.HyperlinkedRelatedField(many=True, view_name='comment_detail', read_only=True)
-
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'slug', 'description', 'published', 'created', 'updated']
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    # user = serializers.ReadOnlyField(source='user.first_name')
-
     class Meta:
         model = Comment
         fields = ['id', 'user', 'post', 'detail', 'created', 'updated']
 
 
 class LikeSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Like
         fields = ['id', 'user', 'comment', 'like', 'created', 'updated']
